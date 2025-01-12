09a2966edfade2f012e2c4af12ee56f9a0418ecb
bthapa99 <90642160+bthapa99@users.noreply.github.com>
2025-01-12 14:47:37 -0500

    Added logic to sort in ascending order

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,11 +1,16 @@
-# add logic to insertion sort here.
-
+# This logic sorts items in ascending order.
+# Need to change the logic so that it is in descending order.
 def insertion_sort(Arr):
-    return Arr
-    #this method should return sorted Arr instead
-
+    for i in range(1, len(Arr)):
+        key = Arr[i]
+        j = i - 1
+        while j >= 0 and Arr[j] > key:
+            Arr[j + 1] = Arr[j]
+            j = j - 1
+        Arr[j + 1] = key
+    return Arr  
 
 arr = [7,5,2,11,65, 23,33,21]    
-print(insertion_sort(arr))
+print("Sorted array in ascending order:", insertion_sort(arr))
 
 
